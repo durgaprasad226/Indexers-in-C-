8757cbd95de3af57569cf5b9809f7f1b4ea41327
durgaprasad226 <durgaprasadmalthumkar@gmail.com>
2023-12-05 16:57:04 +0530

    Initial Commit

---
+++ Indexers/Employee.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    internal class Employee
+    {
+        int Eno;  //class members are private by default
+        string Ename;//we cannot access them in other class
+        double Salary; //If we make them public to access in other class everyone will access which is drawback
+        string Job;  //properties-get, set access
+        string Dname;  //Indexers- provides an access to access values using their index posotion
+        string Location;
+
+        public Employee(int Eno, string Ename, double Salary, string Job, string Dname, string Location)
+        {
+            this.Eno = Eno;
+            this.Ename = Ename;
+            this.Salary = Salary;
+            this.Job = Job;
+            this.Dname = Dname;
+            this.Location = Location;
+        }
+
+        public object this[string name]
+        {
+            get
+            {
+                /*if (index == 0)
+                    return Eno;
+                else if (index == 1)
+                    return Ename;
+                else if (index == 2)
+                    return Salary;
+                else if (index == 3)             //For indexes
+                    return Job;
+                else if (index == 4)
+                    return Dname;
+                else if (index == 5)
+                    return Location;
+                return null;*/
+
+                if (name.ToUpper() == "ENO")
+                    return Eno;
+                else if (name.ToUpper() == "ENAME")
+                    return Ename;
+                else if (name.ToUpper() == "SALARY")    //For strings
+                    return Salary;
+                else if (name.ToUpper() == "JOB")
+                    return Job;
+                else if (name.ToUpper() == "DNAME")
+                    return Dname;
+                else if (name.ToUpper() == "LOCATION")
+                    return Location;
+                return null;
+            }
+
+            set
+            {
+                /*if (index == 0)
+                    Eno = (int)value;
+                else if (index == 1)
+                    Ename = (string)value;
+                else if (index == 2)                //For indexes
+                    Salary = (double)value;
+                else if (index == 3)
+                    Job = (string)value;
+                else if (index == 4)
+                    Dname = (string)value;
+                else if (index == 5)
+                    Location = (string)value;*/
+
+                if (name.ToUpper() == "ENO")
+                    Eno = (int)value;
+                else if (name.ToUpper() == "ENAME")
+                    Ename = (string)value;
+                else if (name.ToUpper() == "SALARY")   //For strings
+                    Salary = (double)value;
+                else if (name.ToUpper() == "JOB")
+                    Job = (string)value;
+                else if (name.ToUpper() == "DNAME")
+                    Dname = (string)value;
+                else if (name.ToUpper() == "LOCATION")
+                    Location = (string)value;
+            }
+        }
+    }
+}
---
+++ Indexers/Indexer.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Indexers
+{
+    internal class Indexer
+    {
+        static void Main(string[] args)
+        {
+            Employee emp=new Employee(1226,"DP", 18000.00, "Developer", "MCB","Hyderabad");
+
+            Console.WriteLine("===================Using string name====================");
+            Console.WriteLine("================get===================");
+            Console.WriteLine("Employee Id : " + emp["Eno"]);
+            Console.WriteLine("Employee Name : " + emp["Ename"]);
+            Console.WriteLine("Employee Salary : " + emp["Salary"]);
+            Console.WriteLine("Employee Job : " + emp["Job"]);
+            Console.WriteLine("Employee Department : " + emp["Dname"]);
+            Console.WriteLine("Location : " + emp["Location"]);
+            Console.WriteLine();
+
+            emp["Eno"] = 2612;
+            emp["Location"] = "Banglore";
+
+            Console.WriteLine("================set===================");
+            Console.WriteLine("Employee Id : " + emp["Eno"]);
+            Console.WriteLine("Employee Name : " + emp["Ename"]);
+            Console.WriteLine("Employee Salary : " + emp["Salary"]);
+            Console.WriteLine("Employee Job : " + emp["Job"]);
+            Console.WriteLine("Employee Department : " + emp["Dname"]);
+            Console.WriteLine("Location : " + emp["Location"]);
+
+
+            /*Console.WriteLine("===================Using Index====================");
+            Console.WriteLine("================get===================");
+            Console.WriteLine("Employee Id : " + emp[0]);
+            Console.WriteLine("Employee Name : " + emp[1]);
+            Console.WriteLine("Employee Salary : " + emp[2]);
+            Console.WriteLine("Employee Job : " + emp[3]);
+            Console.WriteLine("Employee Department : " + emp[4]);
+            Console.WriteLine("Location : " + emp[5]);
+            Console.WriteLine();
+
+            emp[1] = "Durga Prasad";
+            emp[2] = 20000.00;
+
+            Console.WriteLine("================set===================");
+            Console.WriteLine("Employee Id : " + emp[0]);
+            Console.WriteLine("Employee Name : " + emp[1]);
+            Console.WriteLine("Employee Salary : " + emp[2]);
+            Console.WriteLine("Employee Job : " + emp[3]);
+            Console.WriteLine("Employee Department : " + emp[4]);
+            Console.WriteLine("Location : " + emp[5]);*/
+
+            Console.ReadLine();
+        }
+    }
+}
